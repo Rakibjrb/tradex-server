f0001c77ea956cfbe76ab2a0a5712a15f24b8bbb
rakibjrb <rakibjrb572157@gmail.com>
2024-07-04 12:07:05 +0600

    cleared login route and function

--- src/controllers/api/auth/auth.js
+++ src/controllers/api/auth/auth.js
@@ -34,43 +34,4 @@ const addUser = async (req, res, next) => {
   }
 };
 
-const userLogin = async (req, res, next) => {
-  try {
-    const data = req.body;
-    const isUser = await Users.findOne({ email: data.email }, "password");
-
-    if (!isUser) {
-      const error = new Error("User not found on this email");
-      error.status = 403;
-      return next(error);
-    }
-
-    if (!checkLoginPassword(isUser.password, data.password)) {
-      const error = new Error("Invalid password");
-      error.status = 403;
-      return next(error);
-    }
-
-    const correctUser = await Users.findOne(
-      { email: data.email },
-      "username email"
-    );
-
-    const token = createToken({
-      email: correctUser.email,
-      username: correctUser.username,
-    });
-
-    res
-      .cookie("token", token, {
-        secure: true,
-        sameSite: "Strict",
-        httpOnly: true,
-      })
-      .send("Success");
-  } catch (error) {
-    next(error);
-  }
-};
-
-module.exports = { addUser, userLogin };
+module.exports = { addUser };
--- src/routes/auth/route.js
+++ src/routes/auth/route.js
@@ -2,6 +2,5 @@ const authRoute = require("express").Router();
 const { addUser, userLogin } = require("../../controllers/api/auth/auth");
 
 authRoute.post("/api/add-user", addUser);
-authRoute.post("/api/login-user", userLogin);
 
 module.exports = authRoute;
